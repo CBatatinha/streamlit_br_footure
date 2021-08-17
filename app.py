7b968edfcbffed526b8e9f083406cd841337faeb
cbatatinha <caiocbatatinha@gmail.com>
2021-08-16 22:01:30 -0300

    drible fix

--- app.py
+++ app.py
@@ -4196,7 +4196,7 @@ if choice == 'Gráficos times (Partida)':
        prog1 = prog1.append(prog3)
        carry(prog1,prog1,True)
   if grafico == 'Dribles':
-     drible_certo=f_team[(df_team['events']=='TakeOn')&(df_team['outcomeType_displayName']=='Successful')].reset_index(drop=True)
+     drible_certo=df_team[(df_team['events']=='TakeOn')&(df_team['outcomeType_displayName']=='Successful')].reset_index(drop=True)
      drible_errado=df_team[(df_team['events']=='TakeOn')&(df_team['outcomeType_displayName']=='Unsuccessful')].reset_index(drop=True)
      cor_fundo = '#2c2b2b'
      fig, ax = plt.subplots(figsize=(15,10))
